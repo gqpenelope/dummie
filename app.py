2dd6b72c5c8fa513c5d340de7af0f88c0e25b602
gqpenelope <penelopeitzel@gmail.com>
2024-12-05 22:10:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -457,23 +457,7 @@ with tab1:
                 histograma = histog_distr(rendimientos[etf_seleccionado], var_95, cvar_95, f"Distribución de rendimientos para {etf_seleccionado}")
                 st.plotly_chart(histograma)
 
-          
-            st.markdown(
-                """
-                <style>
-                .stMarkdown {
-                    height: 100%;
-                }
-                .metric-container {
-                    display: flex;
-                    flex-direction: column;
-                    justify-content: space-between;
-                    height: 100%;
-                }
-                </style>
-                """,
-                unsafe_allow_html=True
-            )
+
 
 # Tab 2: Portafolios Óptimos
 with tab2:
