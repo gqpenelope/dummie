6dd945d5c550fa7814ee6888cb69f6eec2e38af6
gqpenelope <penelopeitzel@gmail.com>
2024-12-05 20:47:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -414,11 +414,9 @@ with tab1:
                     st.metric(label="CVaR", value=f"{metricas['CVaR']:.2f}")
                 with col9:
                     st.metric(label="Drawdown", value=f"{metricas['Drawdown']:.2f}")
-                
-                st.columns(1)
+
                 col10 = st.columns(1)
-                with col10:
-                    st.metric(label="Momentum", value=f"{metricas['Momentum']:.2f}")
+                st.metric(label="Momentum", value=f"{metricas['Momentum']:.2f}")
                     
 
                 # Histograma de rendimientos
